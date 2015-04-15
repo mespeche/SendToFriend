6e3371fe6835fc9e08c5dbf1c345dd0f5f203486
Franck Allimant <github@allimant.org>
2015-04-15 11:29:00 +0200

    Fixed translations

--- I18n/en_US.php
+++ I18n/en_US.php
@@ -1,9 +1,8 @@
 <?php
+
 return array(
+    'E-mail address of your friend' => 'E-mail address of your friend',
     'Your email address' => 'Your email address',
-    'The email address of your friend' => 'The email address of your friend',
     'Your message' => 'Your message',
-    'A contact wants to share a product with you : ' => 'A contact wants to share a product with you : ',
-    'You can access this product in the following link : ' => 'You can access this product in the following link : ',
-    'This message was sent by : ' => 'This message was sent by : '
+    'Your name' => 'Your name',
 );
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,9 +1,8 @@
 <?php
+
 return array(
+    'E-mail address of your friend' => 'Adresse e-mail de votre ami',
     'Your email address' => 'Votre adresse e-mail',
-    'The email address of your friend' => 'L\'adresse e-mail de votre ami',
     'Your message' => 'Votre message',
-    'A contact wants to share a product with you : ' => 'Un de vos contacts souhaite partager une fiche produit avec vous : ',
-    'You can access this product in the following link : ' => 'Vous pouvez accéder à cette fiche produit en suivant le lien suivant : ',
-    'This message was sent by : ' => 'Ce message a été envoyé par : '
+    'Your name' => 'Votre nom',
 );
